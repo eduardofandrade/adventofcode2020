f91a90f9d5256d07da7ddd4b491c4e68517a8488
Eduardo Andrade <eduardo.andrade@mastercard.com>
2020-12-08 12:03:16 +0000

    Day 06

---
+++ src/main/adventofcode/Day06Solver.kt
@@ -0,0 +1,64 @@
+package adventofcode
+
+import java.io.InputStream
+import java.util.stream.Collectors
+import kotlin.streams.toList
+
+class Day06Solver(stream: InputStream) : Solver {
+
+    private val groups: ArrayList<Group> = arrayListOf()
+
+    init {
+        processInput(stream)
+    }
+
+    private fun processInput(stream: InputStream) {
+        var g: Group? = null
+        stream.bufferedReader().readLines().stream()
+                .forEach { line ->
+                    if (g == null || line.isBlank()) {
+                        g = Group()
+                        groups.add(g!!)
+                    }
+                    val p = Person()
+                    p.addAnswer(line)
+                    g!!.persons.add(p)
+                }
+    }
+
+    override fun getPartOneSolution(): Long {
+        return groups.stream().map { g -> g.getNumberOfUniqueAnswers() }.toList().sum().toLong()
+    }
+
+    override fun getPartTwoSolution(): Long {
+        return groups.stream().map { g -> g.getNumberOfUniqueAnswersThatEveryoneGave() }.toList().sum().toLong()
+    }
+
+    private class Group {
+        val persons: ArrayList<Person> = arrayListOf()
+
+        fun getNumberOfUniqueAnswers(): Int {
+            return persons.stream().flatMap { p -> p.answers.stream() }.collect(Collectors.toSet()).count()
+        }
+
+        fun getNumberOfUniqueAnswersThatEveryoneGave(): Int {
+            val answersPerPerson = persons.stream().map { p -> p.answers }.filter { l -> l.isNotEmpty() }.toList()
+            var answersEveryoneGave: MutableSet<Char>? = if (answersPerPerson.isNotEmpty()) answersPerPerson[0] else mutableSetOf()
+            for (answers in answersPerPerson) {
+                answersEveryoneGave!!.retainAll(answers)
+            }
+            return answersEveryoneGave!!.count()
+        }
+    }
+
+    private class Person {
+        val answers = mutableSetOf<Char>()
+
+        fun addAnswer(answer: String) {
+            for (c in answer) {
+                answers.add(c)
+            }
+        }
+    }
+
+}
---
+++ src/test/adventofcode/Day06SolverTest.kt
@@ -0,0 +1,41 @@
+package adventofcode
+
+import org.junit.Assert
+import org.junit.Test
+
+class Day06SolverTest {
+
+    @Test
+    fun validateExample() {
+        val exampleInput = "abc\n" +
+                "\n" +
+                "a\n" +
+                "b\n" +
+                "c\n" +
+                "\n" +
+                "ab\n" +
+                "ac\n" +
+                "\n" +
+                "a\n" +
+                "a\n" +
+                "a\n" +
+                "a\n" +
+                "\n" +
+                "b"
+
+        val solver = Day06Solver(exampleInput.byteInputStream())
+        Assert.assertEquals(11, solver.getPartOneSolution())
+        Assert.assertEquals(6, solver.getPartTwoSolution())
+    }
+
+    @Test
+    fun printSolutions() {
+        println("========== Day 06 ==========")
+        val solver = Day06Solver(
+                {}.javaClass.getResourceAsStream("day06Input.txt")
+        )
+        println("Part one solution: " + solver.getPartOneSolution())
+        println("Part two solution: " + solver.getPartTwoSolution())
+    }
+
+}
